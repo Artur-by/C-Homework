6a7dee217cba990ac01ec18a123d81309e7d49f9
Artur-by <artur.by@mail.ru>
2022-09-01 11:25:47 +0300

    Task 2 solved

---
+++ Homework4/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите целочисленное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        count = count + number % 10;
+        number = number / 10;
+    }
+    return count;
+}
+
+Console.WriteLine($"Сумма цифр в числе {numb} = {Sum(numb)}");
