f1f6e345280b069b144b649c6a08694a9a492700
Artur-by <artur.by@mail.ru>
2022-09-13 20:58:53 +0300

    Task3 from Homework7 solved

---
+++ Homework7/Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateArray(int lenArray, int widthArray)
+{
+    int[,] newArray = new int[lenArray, widthArray];
+    for (int i = 0; i < lenArray; i++)
+    {
+        for (int j = 0; j < widthArray; j++)
+        {
+            newArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] Average(int[,] array)
+{
+    double[] averegeArray = new double[array.GetLength(1)];
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum = sum + array[j, i];
+        }
+        averegeArray[i] = Math.Round(sum / array.GetLength(0), 2);
+        sum = 0;
+    }
+    return averegeArray;
+}
+
+Console.Write("Введите количество строк массива: ");
+int lenArray = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int widthArray = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = CreateArray(lenArray, widthArray);
+
+PrintArray(myArray);
+
+double[] result = Average(myArray);
+System.Console.WriteLine("Среднее арифметическое каждого столбца:");
+foreach (var item in result)
+{
+    Console.Write($" {item};  ");
+}
